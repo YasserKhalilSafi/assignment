d2ed5327d41970ef85a159ef667d00030e2f4fd5
Yasser Safi <ysafi@souq.com>
2018-01-13 20:29:49 +0200

    empty commit

--- resources/views/index/view.blade.php
+++ resources/views/index/view.blade.php
@@ -1,46 +1,101 @@
-<div class="content"><!-- Content Section -->
-    <div class="explore-rooms margint30 clearfix"><!-- Explore Rooms Section -->
-        <div class="container">
-            <div class="row">
-                @if( $offers == null )
-                    <div class="title-style-2 marginb40 pos-center">
-                        <h2> ... SORRY NO RESULTS FOUND! ...</h2>
-                        <hr>
-                    </div>
-                @else
+<!doctype html>
+    <head>
+        <title>Expedia Assignment</title>
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+            .content {
+                text-align: center;
+            }
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+            * {margin: 0; padding: 0;}
+            div {
+              margin: 20px;
+            }
+            ul {
+              padding-top: 50px;
+              list-style-type: none;
+              width: 500px;
+              margin: auto;
+            }
+            h5 {
+              font: bold 13px/1.5 Helvetica, Verdana, sans-serif;
+            }
+            li img {
+              float: left;
+              margin: 0 20px 0 0;
+            }
+            li p {
+              font: 200 12px/1.5 Georgia, Times New Roman, serif;
+            }
+            li {
+              padding: 10px;
+              overflow: auto;
+            }
+            li:hover {
+              background: #eee;
+              cursor: pointer;
+            }
+            .screenDiv {
+              position: fixed;
+              top: 50%;
+              left: 50%;
+              margin-top: -50px;
+              margin-left: -100px;
+            }
+            .offerTitle {
+              text-align: center;
+              top: 10%;
+              left: 50%;
+              margin-top: -50px;
+              margin-left: -100px;
+            }
+        </style>
+    </head>
+    <body>
+        <div>
+            @if ($offers)
+                <ul>
+                    <div class="offerTitle"><h2>Avilable Offers</h2></div>
                     @foreach($offers->Hotel as $hotel)
-                        <div class="col-lg-4 col-sm-6">
-                            <div class="home-room-box" style="margin-bottom: 85px;">
-                                <div class="room-image">
-                                    <img alt="Room Images" class="img-responsive" style="height: 223px; width: 360px" src="{{$hotel->hotelInfo->hotelImageUrl}}">
-                                    <div class="home-room-details">
-                                        <h5><a href="#">{{$hotel->hotelInfo->hotelName}}</a></h5>
-                                        <div class="pull-left">
-                                            <ul>
-                                                <li><i class="fa fa-calendar"></i></li>
-                                                <li><i class="fa fa-flask"></i></li>
-                                                <li><i class="fa fa-umbrella"></i></li>
-                                                <li><i class="fa fa-laptop"></i></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="room-details" style="height: 130px;">
-                                    <p>{{$hotel->hotelInfo->description}}</p>
-                                </div>
-                                <div class="room-bottom">
-                                    <div class="pull-left"><h4>{{ $hotel->hotelPricingInfo->originalPricePerNight }}$<span class="room-bottom-time">/ Day</span></h4></div>
-                                    <div class="pull-right">
-                                        <div class="button-style-1">
-                                            <a href="#">BOOK NOW</a>
-                                        </div>
-                                    </div>
-                                </div>
+                        <li>
+                            <img alt="Offer" src="{{$hotel->hotelInfo->hotelImageUrl}}">
+                            <h5>{{$hotel->hotelInfo->hotelName}}</h5>
+                            <h5>{{ $hotel->hotelPricingInfo->originalPricePerNight }}$<span>/ Night</span></h5>
+                            <h5>Rating : {{ $hotel->hotelInfo->hotelStarRating}}<span>/5</span></h5>
+                            <h5>Total Rating : {{ $hotel->hotelInfo->hotelReviewTotal}}</h5>
+                            <div class="content">
+                                <a href="#">BOOK THIS OFFER</a>
                             </div>
-                        </div>
+                        </li>
                     @endforeach
-                @endif
-            </div>
+                </ul>
+            @else
+                <div class="screenDiv">
+                    <h2>NO RESULTS FOUND</h2>
+                </div>
+            @endif
         </div>
-    </div>
-</div>
+    </body>
+</html>
+
+
+
+
+
+
